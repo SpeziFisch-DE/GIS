3506849fbaf4d7f01ab36f70beafa0bf99a3eb0a
SpeziFisch-DE <67187041+SpeziFisch-DE@users.noreply.github.com>
2020-11-17 15:16:48 +0100

    p2.1 a6

--- Kapitel2/P2.1/script.js
+++ Kapitel2/P2.1/script.js
@@ -1,3 +1,10 @@
 "use strict";
-console.log("Hallo Welt");
+function a6() {
+    let x = "#";
+    do {
+        console.log(x);
+        x = x + "#";
+    } while (x.length < 8);
+}
+a6();
 //# sourceMappingURL=script.js.map
--- Kapitel2/P2.1/script.ts
+++ Kapitel2/P2.1/script.ts
@@ -1,1 +1,9 @@
-console.log("Hallo Welt");
+function a6(): void {
+    let x: string = "#";
+    do {
+        console.log(x);
+        x = x + "#";
+    } while (x.length < 8);
+}
+
+a6();
