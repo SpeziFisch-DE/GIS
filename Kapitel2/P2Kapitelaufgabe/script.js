3811841e9c0453648a32eb0f64b77fa6f7bcdea0
SpeziFisch-DE <67187041+SpeziFisch-DE@users.noreply.github.com>
2020-12-02 13:15:24 +0100

    selection aside

--- Kapitel2/P2Kapitelaufgabe/script.js
+++ Kapitel2/P2Kapitelaufgabe/script.js
@@ -3,8 +3,25 @@ var Kapitelaufgabe2Script;
 (function (Kapitelaufgabe2Script) {
     //finding HTML Elements
     let imageDiv = document.getElementById("Selection");
+    let imageDivFinished = document.getElementById("finished");
     let buttonSave = document.getElementById("save");
     let buttonCancel = document.getElementById("cancel");
+    let buttonNew = document.getElementById("new");
+    //neccesary for navigation
+    let selectionTypeID = 0; //which Element is to be selected? 0=Klinge 1=Griff 2=Knauf
+    //loading selected Elements aside        
+    let imageKlinge = document.createElement("img");
+    let imageGriff = document.createElement("img");
+    let imageKnauf = document.createElement("img");
+    imageKlinge.src = "";
+    imageGriff.src = "";
+    imageKnauf.src = "";
+    imageKlinge.className = "end";
+    imageGriff.className = "end";
+    imageKnauf.className = "end";
+    imageDivFinished.appendChild(imageKlinge);
+    imageDivFinished.appendChild(imageGriff);
+    imageDivFinished.appendChild(imageKnauf);
     //#region Choosing Element
     function handleClickSetChoice(_event) {
         let images = imageDiv.children;
@@ -15,6 +32,15 @@ var Kapitelaufgabe2Script;
                     prevImg.item(0).className = "";
                 } // unset previous selected Element
                 images.item(i).className = "selected";
+                if (selectionTypeID == 0) {
+                    imageKlinge.src = images.item(i).getAttribute("src");
+                }
+                if (selectionTypeID == 1) {
+                    imageGriff.src = images.item(i).getAttribute("src");
+                }
+                if (selectionTypeID == 2) {
+                    imageKnauf.src = images.item(i).getAttribute("src");
+                }
             }
         }
     }
@@ -35,8 +61,6 @@ var Kapitelaufgabe2Script;
         }
     }
     Kapitelaufgabe2Script.SwordElement = SwordElement;
-    //neccesary for navigation
-    let selectionTypeID = 0; //which Element is to be selected? 0=Klinge 1=Griff 2=Knauf
     function elementsFromJSON(jsonString, requestedTypeID) {
         let arrayFromJASON = JSON.parse(jsonString);
         Object.keys(arrayFromJASON).forEach(key => {
@@ -57,7 +81,9 @@ var Kapitelaufgabe2Script;
         elementsFromJSON(jsonString, selectionTypeID);
     }
     Kapitelaufgabe2Script.readElementsFromJSON = readElementsFromJSON;
-    readElementsFromJSON("data.json");
+    if (window.location.pathname.substring(window.location.pathname.lastIndexOf("/") + 1) == "Select.html") {
+        readElementsFromJSON("data.json");
+    }
     function nextSelection() {
         if (selectionTypeID < 2) {
             selectionTypeID += 1;
@@ -70,40 +96,72 @@ var Kapitelaufgabe2Script;
             readElementsFromJSON("data.json");
         }
         else {
-            window.open("EndSelection.html");
+            window.open("EndSelection.html", "_self");
         }
     }
     //#region Button Click Handler
-    buttonSave.addEventListener("click", handleClickSave);
-    function handleClickSave(_event) {
-        let imgs = document.getElementsByClassName("selected");
-        let imgToSave = "nothing selected!";
-        if (imgs.item(0) != null) {
-            imgToSave = imgs.item(0).getAttribute("src");
+    if (window.location.pathname.substring(window.location.pathname.lastIndexOf("/") + 1) == "Select.html") {
+        buttonSave.addEventListener("click", handleClickSave);
+        function handleClickSave(_event) {
+            let imgs = document.getElementsByClassName("selected");
+            let imgToSave = "nothing selected!";
+            if (imgs.item(0) != null) {
+                imgToSave = imgs.item(0).getAttribute("src");
+            }
+            if (selectionTypeID == 0) {
+                localStorage.setItem("klinge", imgToSave);
+                console.log("saved!");
+                nextSelection();
+            }
+            else if (selectionTypeID == 1) {
+                localStorage.setItem("griff", imgToSave);
+                console.log("saved!");
+                nextSelection();
+            }
+            else if (selectionTypeID == 2) {
+                localStorage.setItem("knauf", imgToSave);
+                console.log("saved!");
+                nextSelection();
+            }
+            else {
+                console.log("Error: Selection could not be saved.");
+            }
+            console.log(imgToSave);
         }
-        if (selectionTypeID == 0) {
-            localStorage.setItem("klinge", imgToSave);
-            console.log("saved!");
-            nextSelection();
+        buttonCancel.addEventListener("click", handleClickCancel);
+        function handleClickCancel(_event) {
+            localStorage.clear();
+            selectionTypeID = 0;
+            let images = imageDiv.children;
+            console.log("to delete" + images.length);
+            for (let i = images.length - 1; i >= 0; i--) {
+                images.item(i).remove();
+                console.log("deleted" + i);
+            }
+            readElementsFromJSON("data.json");
+            console.log("selection canceled!");
         }
-        else if (selectionTypeID == 1) {
-            localStorage.setItem("griff", imgToSave);
-            console.log("saved!");
-            nextSelection();
-        }
-        else if (selectionTypeID == 2) {
-            localStorage.setItem("knauf", imgToSave);
-            console.log("saved!");
-            nextSelection();
-        }
-        else {
-            console.log("Error: Selection could not be saved.");
-        }
-        console.log(imgToSave);
     }
-    buttonCancel.addEventListener("click", handleClickCancel);
-    function handleClickCancel(_event) {
-        console.log("selection canceled!");
+    if (window.location.pathname.substring(window.location.pathname.lastIndexOf("/") + 1) == "EndSelection.html") {
+        buttonNew.addEventListener("click", handleClickNew);
+        function handleClickNew(_event) {
+            localStorage.clear();
+            selectionTypeID = 0;
+            window.open("Select.html", "_self");
+        }
+        //reading local storage
+        let imageKlinge = document.createElement("img");
+        let imageGriff = document.createElement("img");
+        let imageKnauf = document.createElement("img");
+        imageKlinge.src = localStorage.getItem("klinge");
+        imageGriff.src = localStorage.getItem("griff");
+        imageKnauf.src = localStorage.getItem("knauf");
+        imageKlinge.className = "end";
+        imageGriff.className = "end";
+        imageKnauf.className = "end";
+        imageDivFinished.appendChild(imageKlinge);
+        imageDivFinished.appendChild(imageGriff);
+        imageDivFinished.appendChild(imageKnauf);
     }
 })(Kapitelaufgabe2Script || (Kapitelaufgabe2Script = {}));
 //# sourceMappingURL=script.js.map
--- Kapitel2/P2Kapitelaufgabe/script.ts
+++ Kapitel2/P2Kapitelaufgabe/script.ts
@@ -1,9 +1,29 @@
 namespace Kapitelaufgabe2Script {
     //finding HTML Elements
     let imageDiv: HTMLElement = document.getElementById("Selection");
+    let imageDivFinished: HTMLElement = document.getElementById("finished");
     let buttonSave: HTMLElement = document.getElementById("save");
     let buttonCancel: HTMLElement = document.getElementById("cancel");
+    let buttonNew: HTMLElement = document.getElementById("new");
 
+
+    //neccesary for navigation
+    let selectionTypeID: number = 0; //which Element is to be selected? 0=Klinge 1=Griff 2=Knauf
+
+
+    //loading selected Elements aside        
+    let imageKlinge: HTMLImageElement = document.createElement("img");
+    let imageGriff: HTMLImageElement = document.createElement("img");
+    let imageKnauf: HTMLImageElement = document.createElement("img");
+    imageKlinge.src = "";
+    imageGriff.src = "";
+    imageKnauf.src = "";
+    imageKlinge.className = "end";
+    imageGriff.className = "end";
+    imageKnauf.className = "end";
+    imageDivFinished.appendChild(imageKlinge);
+    imageDivFinished.appendChild(imageGriff);
+    imageDivFinished.appendChild(imageKnauf);
 
     //#region Choosing Element
     function handleClickSetChoice(_event: Event): void {
@@ -15,6 +35,15 @@ namespace Kapitelaufgabe2Script {
                     prevImg.item(0).className = "";
                 } // unset previous selected Element
                 images.item(i).className = "selected";
+                if (selectionTypeID == 0) {
+                    imageKlinge.src = images.item(i).getAttribute("src");
+                }
+                if (selectionTypeID == 1) {
+                    imageGriff.src = images.item(i).getAttribute("src");
+                }
+                if (selectionTypeID == 2) {
+                    imageKnauf.src = images.item(i).getAttribute("src");
+                }
             }
         }
     }
@@ -62,8 +91,6 @@ namespace Kapitelaufgabe2Script {
         knauf: SwordElementInterface[];
     }
 
-    //neccesary for navigation
-    let selectionTypeID: number = 0; //which Element is to be selected? 0=Klinge 1=Griff 2=Knauf
 
 
     export function elementsFromJSON(jsonString: string, requestedTypeID: number): void {
@@ -85,52 +112,92 @@ namespace Kapitelaufgabe2Script {
         let jsonString: string = await response.text();
         elementsFromJSON(jsonString, selectionTypeID);
     }
-
-    readElementsFromJSON("data.json");
-
+    if (window.location.pathname.substring(window.location.pathname.lastIndexOf("/") + 1) == "Select.html") {
+        readElementsFromJSON("data.json");
+    }
 
     function nextSelection(): void {
         if (selectionTypeID < 2) {
             selectionTypeID += 1;
             let images: HTMLCollection = imageDiv.children;
             console.log("to delete" + images.length);
-            for (let i: number = images.length - 1; i >= 0 ; i--) {
+            for (let i: number = images.length - 1; i >= 0; i--) {
                 images.item(i).remove();
                 console.log("deleted" + i);
             }
             readElementsFromJSON("data.json");
         } else {
-            window.open("EndSelection.html");
+            window.open("EndSelection.html", "_self");
         }
     }
+
     //#region Button Click Handler
-    buttonSave.addEventListener("click", handleClickSave);
-    function handleClickSave(_event: Event): void {
-        let imgs: HTMLCollection = document.getElementsByClassName("selected");
-        let imgToSave: string = "nothing selected!";
-        if (imgs.item(0) != null) {
-            imgToSave = imgs.item(0).getAttribute("src");
+    if (window.location.pathname.substring(window.location.pathname.lastIndexOf("/") + 1) == "Select.html") {
+        buttonSave.addEventListener("click", handleClickSave);
+        function handleClickSave(_event: Event): void {
+            let imgs: HTMLCollection = document.getElementsByClassName("selected");
+            let imgToSave: string = "nothing selected!";
+            if (imgs.item(0) != null) {
+                imgToSave = imgs.item(0).getAttribute("src");
+            }
+            if (selectionTypeID == 0) {
+                localStorage.setItem("klinge", imgToSave);
+                console.log("saved!");
+                nextSelection();
+            } else if (selectionTypeID == 1) {
+                localStorage.setItem("griff", imgToSave);
+                console.log("saved!");
+                nextSelection();
+            } else if (selectionTypeID == 2) {
+                localStorage.setItem("knauf", imgToSave);
+                console.log("saved!");
+                nextSelection();
+            } else {
+                console.log("Error: Selection could not be saved.");
+            }
+            console.log(imgToSave);
+
         }
-        if (selectionTypeID == 0) {
-            localStorage.setItem("klinge", imgToSave);
-            console.log("saved!");
-            nextSelection();
-        } else if (selectionTypeID == 1) {
-            localStorage.setItem("griff", imgToSave);
-            console.log("saved!");
-            nextSelection();
-        } else if (selectionTypeID == 2) {
-            localStorage.setItem("knauf", imgToSave);
-            console.log("saved!");
-            nextSelection();
-        } else {
-            console.log("Error: Selection could not be saved.");
+        buttonCancel.addEventListener("click", handleClickCancel);
+        function handleClickCancel(_event: Event): void {
+            localStorage.clear();
+            selectionTypeID = 0;
+            let images: HTMLCollection = imageDiv.children;
+            console.log("to delete" + images.length);
+            for (let i: number = images.length - 1; i >= 0; i--) {
+                images.item(i).remove();
+                console.log("deleted" + i);
+            }
+            readElementsFromJSON("data.json");
+            console.log("selection canceled!");
         }
-        console.log(imgToSave);
+
     }
-    buttonCancel.addEventListener("click", handleClickCancel);
-    function handleClickCancel(_event: Event): void {
-        console.log("selection canceled!");
+
+    if (window.location.pathname.substring(window.location.pathname.lastIndexOf("/") + 1) == "EndSelection.html") {
+        buttonNew.addEventListener("click", handleClickNew);
+        function handleClickNew(_event: Event): void {
+            localStorage.clear();
+            selectionTypeID = 0;
+            window.open("Select.html", "_self");
+        }
+
+        //reading local storage
+        let imageKlinge: HTMLImageElement = document.createElement("img");
+        let imageGriff: HTMLImageElement = document.createElement("img");
+        let imageKnauf: HTMLImageElement = document.createElement("img");
+        imageKlinge.src = localStorage.getItem("klinge");
+        imageGriff.src = localStorage.getItem("griff");
+        imageKnauf.src = localStorage.getItem("knauf");
+        imageKlinge.className = "end";
+        imageGriff.className = "end";
+        imageKnauf.className = "end";
+        imageDivFinished.appendChild(imageKlinge);
+        imageDivFinished.appendChild(imageGriff);
+        imageDivFinished.appendChild(imageKnauf);
     }
+
+
+
 
 }
